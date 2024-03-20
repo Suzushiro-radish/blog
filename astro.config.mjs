42c5036166dbe3feeb9388d0ede8485f613c6e89
Daiki <d.takagi.radished@gmail.com>
2024-03-20 22:38:30 +0900

    Change domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://suzushiro-radish.github.io',
-  integrations: [preact()]
+	site: "https://www.suzushiro.net/",
+	integrations: [preact()],
 });
